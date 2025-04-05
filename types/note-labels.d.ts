81b63c5c124c07945cb7f843281f16faf6ef177a
Conor Dowdall <conordowdallmail@gmail.com>
2025-04-05 17:10:01 +0100

    add note label themes for triads and sevenths, and define types for labels override

--- types/note-labels.d.ts
+++ types/note-labels.d.ts
@@ -1,3 +1,5 @@
+import type { PitchInteger } from "./pitch-integer.d.ts";
+
 /** Represents the string value of a musical note name (e.g., "C", "D♭", "E♮", "F♯",). */
 export type NoteName = string;
 
@@ -53,13 +55,61 @@ export interface NoteLabelTheme {
  * The keys of this object are the theme names, and the values are NoteLabelTheme objects.
  */
 export interface NoteLabelThemes {
+  /** e.g. "C", "D♭, ... */
   flat: NoteLabelTheme;
+  /** e.g. "C", "C♯, ... */
   sharp: NoteLabelTheme;
+  /** e.g. "1" for perfect unison, "♭2" for flat second. */
   relative: NoteLabelTheme;
+  /** e.g. "P1" for perfect unison, "m2" for minor second. */
   quality: NoteLabelTheme;
+  /** e.g. "1" for perfect unison, "♭9" for flat second / flat ninth. */
   extension: NoteLabelTheme;
+  /** e.g. "do", "re♭", ... */
   fixedDoFlat: NoteLabelTheme;
+  /** e.g. "do", "do♯", ... */
   fixedDoSharp: NoteLabelTheme;
+  /** e.g. "do", "ra", ...*/
   movableDo: NoteLabelTheme;
+  /** e.g. "la", "te", ... */
   movableLa: NoteLabelTheme;
+  /**
+   * Can be overwritten in a specific NoteSequenceTheme using labelsOverride.
+   * e.g. "M", "m", "o", "+", ...
+   */
+  triad: NoteLabelTheme;
+  /**
+   * Can be overwritten in a specific NoteSequenceTheme using labelsOverride.
+   * e.g. "I", "ii", "V", ...
+   * */
+  romanTriad: NoteLabelTheme;
+  /**
+   * Can be overwritten in a specific NoteSequenceTheme using labelsOverride.
+   * e.g. "M7", "m7", "7", ...
+   */
+  seventh: NoteLabelTheme;
+  /**
+   * Can be overwritten in a specific NoteSequenceTheme using labelsOverride.
+   * e.g. "IM7", "iim7", "V7", ...
+   */
+  romanSeventh: NoteLabelTheme;
 }
+
+/**
+ * Defines a type that represents the names of the available note label themes.
+ */
+export type NoteLabelThemeName = keyof NoteLabelThemes;
+
+/**
+ * Defines a type that represents a Map of PitchInteger to string.
+ * This is used for overriding labels in a specific NoteSequenceTheme.
+ */
+export type LabelsOverrideMap = Map<PitchInteger, string>;
+
+/**
+ * Defines a type that represents the labels override for each note label theme.
+ * This allows for partial overrides of the labels in a specific NoteSequenceTheme.
+ */
+export type LabelsOverride = Partial<
+  Record<NoteLabelThemeName, LabelsOverrideMap>
+>;
