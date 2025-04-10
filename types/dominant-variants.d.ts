096c1db616f961d8ddddecc1d81cba0c14a892e5
Conor Dowdall <conordowdallmail@gmail.com>
2025-04-10 15:01:41 +0100

    refactor: remove unnecessary files

--- types/dominant-variants.d.ts
+++
@@ -1,6 +0,0 @@
-import type { NoteSequenceTheme } from "./note-sequence-theme.d.ts";
-
-export interface DominantVariants {
-  dominant7: NoteSequenceTheme;
-  dominant9: NoteSequenceTheme;
-}
--- types/pitch-integer.d.ts
+++
@@ -1,2 +0,0 @@
-/** Represents the pitch class in integer notation - a number between 0 (C) and 11 (B). */
-export type PitchInteger = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
