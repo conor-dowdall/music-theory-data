5e0353f183187d23af702cdc3340e43c50bb02b3
Conor Dowdall <conordowdallmail@gmail.com>
2025-04-10 15:00:10 +0100

    feat: add getSequenceNoteLabels function for retrieving note labels based on themes

--- src/utils/music-theory-data-utils.ts
+++ src/utils/music-theory-data-utils.ts
@@ -1,5 +1,21 @@
-import type { EnharmonicNotes, NoteName } from "../../types/note-labels.d.ts";
-import type { PitchInteger } from "../../types/pitch-integer.d.ts";
+import type {
+  LabelsOverride,
+  LabelsOverrideMap,
+  PitchInteger,
+} from "../../types/note-sequences.d.ts";
+import type {
+  EnharmonicNotes,
+  NoteLabelGroup,
+  NoteName,
+} from "../../types/note-labels.d.ts";
+import {
+  noteLabelThemes,
+  type NoteLabelThemeName,
+} from "../note-labels/note-label-themes.ts";
+import {
+  noteSequenceThemes,
+  type NoteSequenceThemeName,
+} from "../note-sequences/note-sequences.ts";
 import {
   enharmonicNotesAlt,
   enharmonicNotesUnicode,
@@ -13,16 +29,16 @@ import {
  * @returns The pitch integer (0-11) if found, otherwise undefined.
  */
 export function getIntegerNotation(
-  noteName: NoteName,
+  noteName: NoteName
 ): PitchInteger | undefined {
   const lowerCaseNoteName = noteName.toLowerCase();
 
   const checkEnharmonicNotes = (
-    enharmonicNotes: EnharmonicNotes,
+    enharmonicNotes: EnharmonicNotes
   ): PitchInteger | undefined => {
     for (let index = 0; index < enharmonicNotes.length; index++) {
       const containsNote = enharmonicNotes[index].some(
-        (n) => n.toLowerCase() === lowerCaseNoteName,
+        (n) => n.toLowerCase() === lowerCaseNoteName
       );
       if (containsNote) return index as PitchInteger;
     }
@@ -32,4 +48,32 @@ export function getIntegerNotation(
   const resultAlt = checkEnharmonicNotes(enharmonicNotesAlt);
   if (resultAlt !== undefined) return resultAlt;
   return checkEnharmonicNotes(enharmonicNotesUnicode);
+}
+
+export function getSequenceNoteLabels(
+  noteSequenceThemeName: NoteSequenceThemeName,
+  noteLabelThemeName: NoteLabelThemeName
+): NoteLabelGroup | undefined {
+  const noteSequenceTheme = noteSequenceThemes[noteSequenceThemeName];
+  if (!noteSequenceTheme) return undefined;
+
+  const noteLabelTheme = noteLabelThemes[noteLabelThemeName];
+  if (!noteLabelTheme) return undefined;
+
+  let labels = [...noteLabelTheme.labels] as NoteLabelGroup;
+
+  if ("labelsOverride" in noteSequenceTheme) {
+    labels = labels.map((label, index) => {
+      const labelsOverride = noteSequenceTheme.labelsOverride as LabelsOverride;
+      if (noteLabelThemeName in labelsOverride) {
+        const labelsOverrideMap = labelsOverride[
+          noteLabelThemeName
+        ] as LabelsOverrideMap;
+        return labelsOverrideMap.get(index as PitchInteger) ?? label;
+      }
+      return label;
+    }) as NoteLabelGroup;
+  }
+
+  return labels;
 }
