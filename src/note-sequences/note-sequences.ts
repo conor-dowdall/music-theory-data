09b1db6d31e2d89ffa9ed4261bbfea2331361677
Conor Dowdall <conordowdallmail@gmail.com>
2025-04-10 20:33:50 +0100

    refactor: more efficient noteSequenceTheme and types

--- src/note-sequences/note-sequences.ts
+++ src/note-sequences/note-sequences.ts
@@ -3,22 +3,29 @@ import { dominantVariants } from "./dominant-variants.ts";
 import { majorVariants } from "./major-variants.ts";
 
 /*
+ * A collection of all note sequence themes
+ */
+export const noteSequenceThemes = {
+  diatonicModes,
+  dominantVariants,
+  majorVariants,
+} as const;
+
+/*
  * Defines a type that represents the available note sequence themes.
  */
 export type NoteSequenceThemes = typeof noteSequenceThemes;
 
 /*
  * Defines a type that represents the names of the available
- * note sequence themes.
+ * note sequence theme groups.
  */
-export type NoteSequenceThemeName = keyof NoteSequenceThemes;
+export type NoteSequenceThemeGroup = keyof NoteSequenceThemes;
 
 /*
- * An object containing the available note sequence themes,
- * such as diatonic modes, major variants, and dominant variants.
+ * Defines a type that represents the names of the available
+ * note sequence themes.
  */
-export const noteSequenceThemes = {
-  ...diatonicModes,
-  ...dominantVariants,
-  ...majorVariants,
-};
+export type NoteSequenceThemeName = {
+  [K in keyof NoteSequenceThemes]: keyof NoteSequenceThemes[K];
+}[keyof NoteSequenceThemes];
