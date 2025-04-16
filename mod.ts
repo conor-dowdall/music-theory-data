aa370b929bce79a26313f067273d20d83f535677
Conor Dowdall <conordowdallmail@gmail.com>
2025-04-16 11:38:24 +0100

    docs: update example usage in documentation for music theory data import

--- mod.ts
+++ mod.ts
@@ -8,22 +8,9 @@
  * - Type definitions
  *
  * Example Usage:
- * ```bash
- * deno add jsr:@musodojo/music-theory-data
- * ```
- * ```ts
- * import * as music_theory_data from "@musodojo/music-theory-data";
- * // ...
- * ```
- * ---- OR ----
- *
  * ```ts
  * import * as music_theory_data from "jsr:@musodojo/music-theory-data";
- * // ...
- * ```
  *
- * ```ts
- * import * as music_theory_data from "jsr:@musodojo/music-theory-data";
  * // const majorScale: music_theory_data.PitchInteger[]
  * const majorScale = music_theory_data.diatonicModes.ionian.sequence;
  * console.log("Major Scale Note Sequence", majorScale);
@@ -34,6 +21,15 @@
  * console.log("Melodic Minor Note Sequence Theme", melodicMinorTheme);
  * ```
  *
+ * ---- OR ----
+ *
+ *  * ```bash
+ * deno add jsr:@musodojo/music-theory-data
+ * ```
+ * ```ts
+ * import * as music_theory_data from "@musodojo/music-theory-data";
+ * // ...
+ * ```
  * @module
  */
 
