cde1b23d4a21c0e9b5e49defdf07e0662f8913a5
Conor Dowdall <conordowdallmail@gmail.com>
2025-08-14 13:02:22 +0100

    update

--- src/data/mod.ts
+++ src/data/mod.ts
@@ -1,4 +1,4 @@
 export * from "./chords/mod.ts";
 export * from "./colors/mod.ts";
 export * from "./labels/mod.ts";
-export * from "./pitch-collections/mod.ts";
+export * from "./note-collections/mod.ts";
